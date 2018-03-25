fc20253dd50bec68c9a2a9eb8a27dec7abc7a33a
Auke ten Hoopen <a.tenhoopen@gmail.com>
2018-03-25 21:42:09 +0200

    Added firebase login

--- components/login/login-form.js
+++ components/login/login-form.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, TextInput, Text, TouchableOpacity, StatusBar } from 'react-native';
 
+import * as firebase from 'firebase';
+
 export default class LoginForm extends Component {
+
+    state = {
+        email: '',
+        password: '',
+    };
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user != null) {
+                console.log(user);
+            }
+        });
+    }
+
+    signUpUser = (email, password) => {
+        try {
+            if(this.state.password.length < 6) {
+                alert('please enter at least 6 characters');
+            }
+            
+            firebase.auth().createUserWithEmailAndPassword(email, password);
+        } catch(error) {
+            console.log(error.toString());
+        }
+    };
+
+    loginUser = async (email, password) => {
+        try {
+            const user = await firebase.auth().signInWithEmailAndPassword(email,password);
+            console.log(user);
+        } catch(error) {
+
+        }
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <StatusBar barStyle="light-content" />
                 <TextInput
-                    placeholder="username or email"
+                    placeholder="email"
                     placeholderTextColor="rgba(255,255,255, 0.7)"
                     returnKeyType="next"
                     onSubmitEditing={() => this.passwordInput.focus()}
                     keyboardType="email-address"
                     autoCapitalize="none"
                     autoCorrect={false}
+                    onChangeText={(email) => this.setState({email})}
                     style={styles.input}
                 />
                 <TextInput
@@ -22,12 +60,22 @@ export default class LoginForm extends Component {
                     returnKeyType="go"
                     secureTextEntry={true}
                     style={styles.input}
+                    onChangeText={(password) => this.setState({password})}
                     ref={(input) => this.passwordInput = input}
                 />
 
-                <TouchableOpacity style={styles.buttonContainer}>
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
+                <View >
+                    <TouchableOpacity style={styles.buttonContainer}>
+                        <Text style={styles.buttonText} 
+                            onPress={() => this.loginUser(this.state.email, this.state.password)}
+                        >Login</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttonContainer}>
+                        <Text style={styles.buttonText}
+                            onPress={() => this.signUpUser(this.state.email, this.state.password)}
+                        >Sign up</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -49,12 +97,12 @@ const styles = StyleSheet.create({
 
     buttonContainer: {
         backgroundColor: '#2980b9',
-        paddingVertical: 15
+        paddingVertical: 15,
     },
 
     buttonText: {
         textAlign: 'center',
         color: '#fff',
-        fontWeight: '700'
+        fontWeight: '700',
     }
 });
