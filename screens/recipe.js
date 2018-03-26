fc17d6fdfd3912d8a4e8eaf768897c948e0f2270
Auke ten Hoopen <a.tenhoopen@gmail.com>
2018-03-26 16:42:28 +0200

    Added recipe detail screen

--- screens/recipe.js
+++ screens/recipe.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import firebase, { database } from '../firebase';
+import map from 'lodash/map';
 
 export class RecipeTab extends Component {
 
+    state = {
+        currentUser: null,
+        recipe: null,
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((currentUser) => {
+            this.setState({ currentUser });
+
+            const { recipeId } = this.props.navigation.state.params;
+            console.log(recipeId);
+
+            database.ref(`/recipes/${recipeId}`).on('value', (snapshot) => {
+                console.log(snapshot);
+
+                this.setState({ recipe: snapshot.val() });
+            });
+        });
+    }
+
     render() {
+        const { recipe } = this.state;
+
         return (
             <View style={styles.container}>
-                <Text>Recipe tab</Text>
+                {recipe && (
+                    <View>
+                        <Text>{recipe.title}</Text>
+                        {map(recipe.ingredients, (ingredient) => {
+                            return <Text key={ingredient.name}>{ingredient.name}</Text>
+                        })}
+                    </View>
+                )}
             </View>
         )
     }
