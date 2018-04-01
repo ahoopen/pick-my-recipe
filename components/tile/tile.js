95837b565e446c8b459b93c5901ccafd7d028931
Auke ten Hoopen <a.tenhoopen@gmail.com>
2018-04-01 21:26:27 +0200

    Added tile component

---
+++ components/tile/tile.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { StyleSheet, TouchableWithoutFeedback, Animated, Easing } from 'react-native';
+
+export class Tile extends Component {
+
+    componentWillMount() {
+        this.animatedValue = new Animated.Value(1);
+    }
+
+    handlePressIn = () => {
+        Animated.spring(this.animatedValue, {
+            toValue: .9,
+            duration: 300,
+            easing: Easing.easeOutBack
+        }).start();
+    };
+
+    handlePressOut = () => {
+        Animated.spring(this.animatedValue, {
+            toValue: 1,
+            friction: 3,
+            tension: 40
+        }).start();
+    };
+
+    render() {
+        const animatedStyle = {
+            transform: [{
+                scale: this.animatedValue
+            }]
+        };
+
+        return (
+            <TouchableWithoutFeedback
+                onPressIn={this.handlePressIn}
+                onPressOut={this.handlePressOut}
+            >
+                <Animated.View
+                    style={[{
+                        flex: 1,
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }, animatedStyle]}
+                >
+                    {this.props.children}
+                </Animated.View>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
